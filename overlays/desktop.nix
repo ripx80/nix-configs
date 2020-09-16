60bc77f29b1c57dc195ca1b7ea66ddc29aef0206
drittweiler <ripx80@protonmail.com>
2020-09-16 08:25:50 +0200

    add xorg

--- overlays/desktop.nix
+++ overlays/desktop.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ hardware, config, pkgs, ... }:
 
 {
 
@@ -9,8 +9,22 @@
     google-chrome
     killall
     file
+    #xorg
+    #xorg.xorgserver
+    # input
+    #xorg.xf86inputevdev
+    #xorg.xf86inputsynaptics
+    #xorg.xf86inputlibinput
+    # drivers
+    #xorg.xf86videointel
+    #xorg.xf86videoati
+    #xorg.xf86videonouveau
+    # window manager
     openbox
   ];
+  #DRI acceleration
+  #hardware.opengl.enable = true;
+  #hardware.opengl.driSupport = true;
 
 #   rip = {
 #     openbox.enable = true;
@@ -18,8 +32,8 @@
 #   };
 
   virtualisation.docker.enable = true;
-
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
 
-
+  services.xserver.enable = true;
+  services.xserver.displayManager.startx.enable = true;
 }
