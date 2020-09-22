bc52714d912ed73812d3741bb1e0b179e141c06d
drittweiler <ripx80@protonmail.com>
2020-09-22 16:05:33 +0200

    c

--- common/users/rip/base.nix
+++ common/users/rip/base.nix
@@ -18,4 +18,19 @@
     path = "…";
   };
 
+  programs.git = {
+    enable = true;
+    userName = "ripx80";
+    userEmail = "ripx80@protonmail.com";
+    push.default = "matching";
+    aliases = {
+        permission-reset = "!git diff -p -R --no-color | grep -E \"^(diff|(old|new) mode)\" --color=never | git apply";
+    };
+    extraConfig = { http { sslCAinfo = "/etc/ssl/certs/ca-certificates.crt"; }; };
+    # signing = {
+    #   key = "me@yrashk.com";
+    #   signByDefault = true;
+    # };
+  };
+
 }
