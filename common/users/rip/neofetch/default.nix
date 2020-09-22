a0e726c8a345a9cc40c410b8a2ddf5f06e4f8c5a
drittweiler <ripx80@protonmail.com>
2020-09-22 16:42:04 +0200

    alla

--- common/users/rip/neofetch/default.nix
+++ common/users/rip/neofetch/default.nix
@@ -3,7 +3,7 @@
 {
     home = {
         packages = with pkgs; [ neofetch ];
-        file.".config/neofetch/config.confh".source = ./config.conf;
+        file.".config/neofetch/config.conf".source = ./config.conf;
     };
 
 }
