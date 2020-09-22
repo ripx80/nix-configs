92043aa4d853b4d6b1316b78b922fd2aa1dfab59
drittweiler <ripx80@protonmail.com>
2020-09-22 16:08:42 +0200

    a

--- common/users/rip/base.nix
+++ common/users/rip/base.nix
@@ -22,11 +22,13 @@
     enable = true;
     userName = "ripx80";
     userEmail = "ripx80@protonmail.com";
-    push.default = "matching";
     aliases = {
         permission-reset = "!git diff -p -R --no-color | grep -E \"^(diff|(old|new) mode)\" --color=never | git apply";
     };
-    extraConfig = { http = { sslCAinfo = "/etc/ssl/certs/ca-certificates.crt"; }; };
+    extraConfig = {
+        http = { sslCAinfo = "/etc/ssl/certs/ca-certificates.crt"; };
+        push = { default = "matching"; };
+    };
     # signing = {
     #   key = "me@yrashk.com";
     #   signByDefault = true;
