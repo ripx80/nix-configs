a51fb2b32b4884877b55a2073ea72293b5e84ad7
drittweiler <ripx80@protonmail.com>
2020-09-22 15:23:25 +0200

    more home

--- common/users/rip/base.nix
+++ common/users/rip/base.nix
@@ -9,9 +9,24 @@
     EDITOR = "nano";
   };
 
+  home.packages = [
+    pkgs.unzip
+  ];
+
   programs.home-manager = {
     enable = true;
     path = "…";
+  };
+
+  programs.git = {
+    package = pkgs.gitAndTools.gitFull;
+    enable = true;
+    userName = "ripx80";
+    userEmail = "ripx80@protonmail.com";
+    # signing = {
+    #   key = "me@yrashk.com";
+    #   signByDefault = true;
+    # };
   };
 
 }
