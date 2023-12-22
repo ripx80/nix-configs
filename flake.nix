e95591eb76efc01822c2113b35e50c39db7e05e8
ripx80 <ripx80@protonmail.com>
2023-12-22 11:28:55 +0100

    fix hm master branch

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
       #url = "github:nix-community/home-manager"; # unstable
       #inputs.nixpkgs.follows = "nixpkgs-unstable";
